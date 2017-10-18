526bf431e11c910ab1ed4c4651ec558c3f3835fa
Tima <temirlanhaiyrmu@gmail.com>
2017-10-19 00:32:25 +0800

    bugs fixing

--- Battleships C#/src/DeploymentController.cs
+++ Battleships C#/src/DeploymentController.cs
@@ -17,6 +17,7 @@ static class DeploymentController
 	private const int SHIPS_HEIGHT = 90;
 
 	private const int SHIPS_WIDTH = 300;
+
 	private const int TOP_BUTTONS_TOP = 72;
 
 	private const int TOP_BUTTONS_HEIGHT = 46;
